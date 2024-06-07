218d6a6cabfcbc063b1933af50ab451949c81b1a
graziellecarvalho <graziellepcarvalho@gmail.com>
2024-06-07 06:36:35 -0400

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/graziellecarvalho",
+  base: "/book-collection",
   plugins: [react()],
   resolve: {
     alias: {
