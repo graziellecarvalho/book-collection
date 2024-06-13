34bab88a1d94211a1104379b2973b12cfffc6800
graziellecarvalho <graziellepcarvalho@gmail.com>
2024-06-13 12:07:37 -0400

    Unplugin auto import setup and testing on BookCollectionTable component

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AutoImport({ 
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.json$/, // .json
+        /\.ts$/, // .ts
+      ],
+      // global imports to register
+      imports: [],
+      dirs: [
+        './src/**',
+      ],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+      // Enable auto import by filename for default module exports under directories
+      defaultExportByFilename: false,
+      // dts: './auto-imports.d.ts',
+      dts: true
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
